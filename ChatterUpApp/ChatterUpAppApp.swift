ccd3e6104c41201e16496e5532618bead1af4f9d
Adi Amoyal <adi.amoyal02@gmail.com>
2024-04-26 14:14:55 -0700

    Initial Commit

---
+++ ChatterUpApp/ChatterUpAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatterUpAppApp.swift
+//  ChatterUpApp
+//
+//  Created by Adi Amoyal on 26/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatterUpAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
